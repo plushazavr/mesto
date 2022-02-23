4df4fb951a3411871c6248f2aa85fce4751591b0
Ekaterina Kazantseva <plushazavr.time@gmail.com>
2022-02-23 20:51:15 +0300

    style: вынести cards

---
+++ scripts/cards.js
@@ -0,0 +1,27 @@
+//карточки "коробки"
+const initialCards = [
+  {
+    name: 'Новогодний наряд',
+    link: 'images/dog-1.jpg'
+  },
+  {
+    name: 'Пузико',
+    link: 'images/dog-2.jpg'
+  },
+  {
+    name: 'Ковбой',
+    link: 'images/dog-4.jpg'
+  },
+  {
+    name: 'Спим в дороге',
+    link: 'images/dog-5.jpg'
+  },
+  {
+    name: 'Дьявол',
+    link: 'images/dog-6.jpg'
+  },
+  {
+    name: 'Кроха',
+    link: 'images/dog-7.jpg'
+  }
+];

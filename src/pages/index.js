62c91bd16149168c4bfcaccdb24386057ff39bbd
Ekaterina Kazantseva <dr.kazantseva.ekaterina@gmail.com>
2022-04-19 11:10:09 +0300

    webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,33 @@
-import { Card } from '../scripts/Card.js';
+import '../pages/index.css';
+
+import { Card } from '../components/Card.js';
 import FormValidator from '../scripts/FormValidator.js';
 
 //карточки "коробки"
 const initialCards = [
   {
-    name: 'Новогодний наряд',
-    link: 'images/dog-1.jpg'
+    name: 'Зубатка',
+    link: 'https://i.pinimg.com/originals/ee/e7/94/eee79455b14006ae5fb0e73e85a42077.jpg'
   },
   {
-    name: 'Пузико',
-    link: 'images/dog-2.jpg'
+    name: 'Лежачий',
+    link: 'https://i.pinimg.com/originals/e3/37/2e/e3372ea57712e4772284982f9bae167c.jpg'
   },
   {
-    name: 'Ковбой',
-    link: 'images/dog-4.jpg'
+    name: 'Почти котопес',
+    link: 'https://i.pinimg.com/originals/60/6d/27/606d27e0e32e2e2ffbae7b3080d3b600.jpg'
   },
   {
-    name: 'Спим в дороге',
-    link: 'images/dog-5.jpg'
+    name: 'Возьми на постель',
+    link: 'https://i.pinimg.com/originals/8e/5a/c4/8e5ac45f7b5836898a6058f38f8d5cf5.jpg'
   },
   {
     name: 'Дьявол',
-    link: 'images/dog-6.jpg'
+    link: 'https://i.pinimg.com/564x/da/19/f7/da19f7b6d4f306e00b07953249024183.jpg'
   },
   {
-    name: 'Кроха',
-    link: 'images/dog-7.jpg'
+    name: 'Я у мамы дурачок',
+    link: 'https://i.pinimg.com/originals/50/bd/bd/50bdbda668486184990c980e9d766bdc.jpg'
   }
 ];
 
